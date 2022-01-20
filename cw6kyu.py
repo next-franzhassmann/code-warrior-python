e21ebf7f375a87de992c12ccd855ab5614d98126
Franz <franz.hassmann3.next@bbva.com>
2022-01-20 19:12:20 +0100

    (refact) Modulos de katas de 6kyu y sus test asociados

--- cw6kyu.py
+++ cw6kyu.py
@@ -55,3 +55,162 @@ def alphabet_position(text):
             l_result.append(str(encoder[c]))
     return ' '.join(l_result)  
 
+#Does my number look big in this?
+def narcissistic( value ):
+    """A Narcissistic Number is a positive number which is the sum of its own digits, 
+    each raised to the power of the number of digits in a given base. 
+    In this Kata, we will restrict ourselves to decimal (base 10)
+    For example, take 153 (3 digits), which is narcisstic: 
+    1^3 + 5^3 + 3^3 = 1 + 125 + 27 = 153
+    and 1652 (4 digits), which isn't:
+    1^4 + 6^4 + 5^4 + 2^4 = 1 + 1296 + 625 + 16 = 1938
+    """
+
+    value_tuple=tuple(str(value))
+    exp=len(value_tuple)
+    calc=0
+    for i in range(len(value_tuple)):
+        calc=calc+int(value_tuple[i])**exp
+    return calc==value
+
+#Delete occurrences of an element if it occurs more than n times
+def delete_nth(order,max_e):
+    """ Given a list lst and a number N, create a new list that contains each number 
+    of lst at most N times without reordering. 
+    For example if N = 2, and the input is [1,2,3,1,2,1,2,3], 
+    you take [1,2,3,1,2], drop the next [1,2] since this would lead 
+    to 1 and 2 being in the result 3 times, and then take 3, which leads to [1,2,3,1,2,3].
+    """
+    list_number={}
+    for i in range(len(order)):
+        list_number[order[i]]=order.count(order[i])
+    print(list_number)
+    acciones=[] #Lista de acciones por elemento 'M'antener y 'B'orrar
+    for j in range(-1,-len(order)-1,-1):
+        if list_number[order[j]] > max_e:
+            acciones.insert(0,'B')
+            list_number[order[j]] -= 1
+        else:
+            acciones.insert(0,'M')
+    list_res=[]
+    for k in range (len(order)):
+        if acciones[k]=='M':
+            list_res.append(order[k])
+        k +=1
+    return list_res
+
+#Valid braces
+
+def validBraces(string):
+    """Write a function that takes a string of braces, and determines 
+    if the order of the braces is vac1d. It should return true if the string is vac1d, 
+    and false if it's invac1d.
+    This Kata is similar to the Vac1d Parentheses Kata, but introduces new characters: 
+    brackets [], and curly braces {}. Thanks to @arnedag for the idea!
+    All input strings will be nonempty, and will only consist of parentheses, 
+    brackets and curly braces: ()[]{}.
+    """
+
+    def stack_init ():
+        return []
+
+    def stack_push (s:list,e):
+        s.append(e)
+        return s
+    
+    def stack_pop (s:list):
+        if len(s)>=1:
+            s.pop()
+        else:
+            print("Error, la pila ya está vacía")
+    
+    def stack_top(s:list):
+        if len(s)>0:
+            return s[-1]
+        else:
+            print("Error, la pila ya está vacía")
+
+    def cierra_parentesis(s:list):
+        c1=stack_top(s)
+        stack_pop(s)
+        c2=stack_top(s)
+        stack_pop(s)
+        if (c1==')' and c2=='(') or (c1==']' and c2=='[') or (c1=='}' and c2=='{'):
+            pass
+        else:
+            stack_push(s,c2)
+            stack_push(s,c1)
+        
+    stack=stack_init()
+    i=0
+    while  i<=len(string)-1:
+        stack_push(stack,string[i])
+        if len(stack)>=2:
+            cierra_parentesis(stack)
+        i+=1
+    return len(stack)==0
+
+def dig_pow(n, p):
+    """Given a positive integer n written as abcd... (a, b, c, d... being digits) and a 
+    positive integer p
+    we want to find a positive integer k, if it exists, such as the sum of the digits  
+    of n taken to the successive powers of p is equal to k * n.
+    In other words:
+    Is there an integer k such as : (a ^ p + b ^ (p+1) + c ^(p+2) + d ^ (p+3) + ...) = n * k
+    If it is the case we will return k, if not return -1.
+    #ex dig_pow(695, 2) should return 2 since 6² + 9³ + 5⁴= 1390 = 695 * 2
+    """
+    
+    num_str=str(n)
+    acc=0
+    exp=p
+    for dig in num_str:
+        acc+=int(dig)**exp
+        exp+=1
+    if acc % n == 0:
+        k=int(acc/n)
+        return k
+    else:
+        return -1
+
+##Highest Scoring Word
+def high(x):
+    """Given a string of words, you need to find the highest scoring word.
+    Each letter of a word scores points according to its position in the 
+    alphabet: a = 1, b = 2, c = 3 etc.
+    You need to return the highest scoring word as a string.
+    If two words score the same, return the word that appears earliest in the original 
+    string. All letters will be lowercase and all inputs will be valid.
+    """
+
+    def word_scoring(p):
+        acc=0
+        for c in p:
+            #print(ord('a')) -- 97
+            acc += ord(c)-96
+        return acc
+        
+    lista_palabras=x.split()
+    lista_scoring=[]
+    for p in lista_palabras:
+        lista_scoring.append(word_scoring(p))
+    i_max=lista_scoring.index(max(lista_scoring))
+    return lista_palabras[i_max]
+
+#Find the unique number
+def find_uniq(list):
+    """ There is an array with some numbers. All numbers are equal except for one. 
+    Try to find it!
+    It’s guaranteed that array contains at least 3 numbers.
+    The tests contain some very huge arrays, so think about performance.
+    """
+    vals_distintos=set(list)
+    dic_contador={}
+    for e in vals_distintos:
+        dic_contador[e]=list.count(e)
+    for k in dic_contador.keys():
+        if dic_contador[k] == 1:
+            return k
+        
+    
+
--- test_6kyu.py
+++ test_6kyu.py
@@ -39,3 +39,70 @@ from cw6kyu import alphabet_position
 def test_alphabet_position1_():
     assert alphabet_position("The sunset sets at twelve o' clock.") \
         == "20 8 5 19 21 14 19 5 20 19 5 20 19 1 20 20 23 5 12 22 5 15 3 12 15 3 11"
+
+#Does my number look big in this?
+from cw6kyu import narcissistic
+
+def test_narcissistic_1():
+    assert narcissistic(153)==True
+def test_narcissistic_2():
+    assert narcissistic(1938)==False
+
+#Delete occurrences of an element if it occurs more than n times
+from cw6kyu import delete_nth
+
+def test_delete_nth_1():
+    assert delete_nth([1,1,1,1],2)==[1,1]
+def test_delete_nth_2():
+    assert delete_nth([20,37,20,21],1)==[20,37,21]
+def test_delete_nth_3():
+    assert delete_nth([1,2,3,1,2,1,2,3],2)==[1,2,3,1,2,3]
+
+#Valid braces
+from cw6kyu import validBraces
+
+def test_validbraces_1():
+    assert validBraces("(){}[]") == True
+def test_validbraces_2():
+    assert validBraces("([{}])") == True
+def test_validbraces_3():
+    assert validBraces("(}") == False
+def test_validbraces_4():
+    assert validBraces("[(])") == False  
+def test_validbraces_5():
+    assert validBraces("[({})](]") == False
+    
+#Playing with digits
+from cw6kyu import dig_pow
+
+"""
+dig_pow(89, 1) should return 1 since 8¹ + 9² = 89 = 89 * 1
+dig_pow(92, 1) should return -1 since there is no k such as 9¹ + 2² equals 92 * k
+dig_pow(695, 2) should return 2 since 6² + 9³ + 5⁴= 1390 = 695 * 2
+dig_pow(46288, 3) should return 51 since 4³ + 6⁴+ 2⁵ + 8⁶ + 8⁷ = 2360688 = 46288 * 51
+"""
+
+def test_digpow_1():
+    assert dig_pow(89,1)==1
+def test_digpow_2():
+    assert dig_pow(92,1)==-1
+def test_digpow_3():
+    assert dig_pow(695,2)==2
+def test_digpow_4():
+    assert dig_pow(46288,3)==51
+
+##Highest Scoring Word
+from cw6kyu import high
+
+def test_high():
+    assert high('what time are we climbing up the volcano')=="volcano"
+
+#Find the unique number
+from cw6kyu import find_uniq
+
+def test_find_uniq_1():
+    assert find_uniq([ 1, 1, 1, 2, 1, 1 ]) == 2
+def test_find_uniq_2():
+    assert find_uniq([ 0, 0, 0.55, 0, 0 ]) == 0.55
+
+
